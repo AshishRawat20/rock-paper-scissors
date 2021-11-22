65f9de614f468392311e2aeeb24df37224d10b42
Ashish Rawat <rawata123789@gmail.com>
2021-11-22 16:53:03 +0530

    Added button functionality and some css

--- script.js
+++ script.js
@@ -7,48 +7,78 @@ function computerChoice(){
     return choices[choice];
 }
 
-function playSingleRound(computerPlay, playerPlay){
+function playSingleRound(playerPlay){
+    let computerPlay = computerChoice();
     playerPlay = playerPlay.toLowerCase();
+    let returnString = `Computer chose ${computerPlay} `;
     if((computerPlay === 'rock' && playerPlay == 'scissors') || (computerPlay === 'scissors' && playerPlay === 'paper') || (computerPlay === 'paper' && playerPlay === 'rock')){
-        //console.log("You lost! :(");
-        return -1;
+        computerCount++;
+        returnString+="You lost! :(";
     }
     else if(computerPlay === playerPlay){
-        //console.log("Its a draw");
-        return 0;
+        playerCount++;
+        computerCount++;
+        returnString+="Its a draw";
     }
     else{
-        //console.log("You won");
-        return 1;
+        playerCount++;
+        returnString+="You won";
     }
+    return returnString;
 }
 
-function game(){
-    let playerCount = 0, computerCount = 0;
-    for(var i=0; i<5; i++){
-        const playerPlay = prompt("Choose between rock, paper and scissors").toLowerCase();
-        const computerPlay = computerChoice().toLowerCase();
-        var val = playSingleRound(computerPlay, playerPlay);
-        if(val === 1){
-            playerCount++;
-        }
-        else if(val == -1){
-            computerCount++;
-        }
-        else{
-            playerCount++;
-            computerCount++;
-        }
-    }
-    console.log("Score\n"+playerCount +" : "+ computerCount);
-    if(playerCount > computerCount){
-        return "You Won";
-    }
-    else if(playerCount === computerCount){
-        return "It's a draw";
-    }
-    else return "You Lost!";
-}
+let playerCount = 0, computerCount = 0;
+// function game(){
+//     let playerCount = 0, computerCount = 0;
+//     for(var i=0; i<5; i++){
+//         const playerPlay = prompt("Choose between rock, paper and scissors").toLowerCase();
+//         const computerPlay = computerChoice().toLowerCase();
+//         var val = playSingleRound(computerPlay, playerPlay);
+//         if(val === 1){
+//             playerCount++;
+//         }
+//         else if(val == -1){
+//             computerCount++;
+//         }
+//         else{
+//             playerCount++;
+//             computerCount++;
+//         }
+//     }
+//     console.log("Score\n"+playerCount +" : "+ computerCount);
+//     if(playerCount > computerCount){
+//         return "You Won";
+//     }
+//     else if(playerCount === computerCount){
+//         return "It's a draw";
+//     }
+//     else return "You Lost!";
+// }
 
+const choice = document.querySelectorAll(`.choice`);
+const result = document.querySelector(`.result`);
+// console.log(choice);
 
-console.log(game())
+choice.forEach(ch => {
+    ch.addEventListener("click", ()=>{
+        result.textContent = playSingleRound(ch.textContent);
+        playerScore.textContent = playerCount.toString();
+        computerScore.textContent = computerCount.toString();
+        if(playerCount == 5 || computerCount == 5){
+            if(playerCount == 5)
+            result.textContent = "You won the game of 5! Refresh page to play again.";
+            if(computerCount == 5){
+                result.textContent = "You lost the game of 5 :( Refresh page to play again.";
+            }
+            playerCount = 0;
+            computerCount = 0;
+            
+        }
+    });
+});
+
+const playerScore = document.querySelector(`.player-score`);
+const computerScore = document.querySelector(`.computer-score`);
+
+// console.log(playerScore);
+// console.log(computerScore);
